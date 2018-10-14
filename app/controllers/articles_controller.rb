14558e00b016ff8762c7b09adaf3928f111066db
tbrewste <tbrewste@uncc.edu>
2018-10-14 01:18:10 +0000

    List all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,19 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
     def new
     end
     
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params[:article])
+        
+        @article.save
+        redirect_to @article
     end
 end
+
+private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
